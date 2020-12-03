aa40fe2b8e3325935ed02160d3607e3407a6821f
Durumi <DrummyCrane@users.noreply.github.com>
2020-12-03 14:02:45 +0900

    Create new project

---
+++ PasswordServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PasswordServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
